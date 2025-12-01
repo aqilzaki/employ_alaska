4ee2ff73fc1209574f4344d817fe85b31fb46b71
teddyedwar29 <teddyedwar@gmail.com>
2025-12-01 14:24:36 +0700

    CRUD done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import EmployeeDashboard from './admin/pages/EmployeeDashboard';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Rute baru untuk halaman data karyawan */}
+        <Route path="/admin/karyawan" element={<EmployeeDashboard />} />
+        
+        {/* Rute yang sudah ada untuk dashboard */}
+        <Route path="/admin/dashboard" element={<EmployeeDashboard />} />
+
+        <Route path="/" element={<Navigate to="/admin/dashboard" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}

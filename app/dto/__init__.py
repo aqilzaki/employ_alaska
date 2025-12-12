47a77d8876e811715b2e50b9a6444b06fbdd1bb7
aqil <mzaki5457@gmail.com>
2025-12-13 06:56:09 +0700

    update

--- app/dto/__init__.py
+++ app/dto/__init__.py
@@ -17,8 +17,41 @@ from app.dto.karyawan_dto import (
     karyawan_update_schema
 )
 
+from app.dto.status_pernikahan_dto import (
+    status_pernikahan_schema,
+    status_pernikahan_list_schema,
+    status_pernikahan_create_schema,
+    status_pernikahan_update_schema
+)
+
+from app.dto.departemen_dto import (
+    departemen_schema,
+    departemen_list_schema,
+    departemen_create_schema,
+    departemen_update_schema
+)   
+
+from app.dto.agama_dto import (
+    agama_schema,
+    agama_list_schema,
+    agama_create_schema,
+    agama_update_schema
+)
+
 __all__ = [
     'jabatan_schema', 'jabatan_list_schema', 'jabatan_create_schema', 'jabatan_update_schema',
     'status_kerja_schema', 'status_kerja_list_schema', 'status_kerja_create_schema', 'status_kerja_update_schema',
     'karyawan_schema', 'karyawan_list_schema', 'karyawan_create_schema', 'karyawan_update_schema'
+    'status_pernikahan_schema',
+    'status_pernikahan_list_schema',
+    'status_pernikahan_create_schema',
+    'status_pernikahan_update_schema',
+    'departemen_schema',
+    'departemen_list_schema',
+    'departemen_create_schema',
+    'departemen_update_schema',
+    'agama_schema',
+    'agama_list_schema',
+    'agama_create_schema',
+    'agama_update_schema'
 ]
